2f56eda28abc99ba2526c17d0c928d23ba005b89
yu <peacetomori1118@gmail.com>
2021-05-04 01:10:28 +0900

    dev superSearcher

--- search.go
+++ search.go
@@ -13,6 +13,9 @@ import (
 	"net/url"
 	"os"
 	"strings"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 )
 
 var num = flag.String("n", "10", "検索結果の数")
@@ -62,8 +65,62 @@ func wordsParser(words string) (result string) {
 	return result
 }
 
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
 func crowlAndShot(urlstring string) {
-	fmt.Println(urlstring)
 	agoutiDriver := agouti.ChromeDriver()
 	agoutiDriver.Start()
 	defer agoutiDriver.Stop()
@@ -82,7 +139,7 @@ func crowlAndShot(urlstring string) {
 	}
 }
 
-func main() {
+func init() {
 	flag.Parse()
 	file, err := os.Open(".env")
 	if err != nil {
@@ -105,7 +162,9 @@ func main() {
 			API_KEY = value
 		}
 	}
+}
 
+func main() {
 	num_of_results_param := "&num=" + *num
 	start_param := "&start=" + *start
 	lang_ja_param := "&lr=lang_ja"
@@ -123,6 +182,7 @@ func main() {
 	}
 	defer resp.Body.Close()
 	content, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(content))
 	responds := new(GSEResponse)
 	err = json.NewDecoder(bytes.NewBuffer(content)).Decode(responds)
 	if err != nil {
